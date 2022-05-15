2d8530e31c8da6ab064f16b7d0e10eb4a79388e2
Darshi <darshi373@gmail.com>
2022-05-15 23:42:56 +0530

    media path added and admin changes

--- ssikart/ssikart/settings.py
+++ ssikart/ssikart/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import django
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,6 +43,8 @@ INSTALLED_APPS = [
     'home',
     'category',
     'accounts',
+    #Third Party Packages
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +119,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+#Auth User
+AUTH_USER_MODEL = 'accounts.Account'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -122,6 +129,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = []
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 print(STATIC_ROOT)
 print("BASE:", BASE_DIR)
 
